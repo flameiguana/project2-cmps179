68aa069dfcb7b750aff70ea6996f9d199c387668
flameiguana <flameiguana@gmail.com>
2013-05-27 00:58:42 -0700

    texture mapping suckas

---
+++ scripts/meshes.js
@@ -0,0 +1,29 @@
+//images are oldest to newest
+function baseChunk(unit, color, image, genre){
+
+	var ALBUM_COUNT = 4;
+	var depth = unit;
+	var width = unit * ALBUM_COUNT;
+	var height = unit;
+	var coverMaterials = [];
+
+
+	//left, right, top, bottom, front, and back
+	var baseColor = new THREE.MeshLambertMaterial({color: color});
+	var coverMaterial = new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture(image)});
+
+	var materials = [
+		baseColor,
+		baseColor,
+		baseColor,
+		baseColor,
+		coverMaterial,
+		baseColor
+	];
+
+	var geometry = new THREE.CubeGeometry(width, height, depth, ALBUM_COUNT, 1, 1);
+	var mainMaterial = new THREE.MeshFaceMaterial(materials);
+	mainMaterial.wrapAround = true;
+	var mesh = new THREE.Mesh(geometry, mainMaterial);
+	return mesh;
+}
--- scripts/visualization.js
+++ scripts/visualization.js
@@ -1,7 +1,6 @@
 console.log("Loaded"); 
 
 var $container = $('#container');
-
 var cw = $container.width();
 //Set container height to width
 //16:9 aspect ratio
@@ -9,7 +8,8 @@ $container.css({'height': (cw * .5625) +'px'});
 
 var WIDTH = $container.width();
 var HEIGHT = $container.height();
-
+var mouseX = 0; 
+var mouseY = 0;
 
 var VIEW_ANGLE = 45;
 var ASPECT = WIDTH/HEIGHT;
@@ -17,48 +17,59 @@ var NEAR = 0.1;
 var FAR = 1000;
 
 
+/*-------------Set up scene essentials --------------- */
+
 //Change this to support Safari
 var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera (
-	VIEW_ANGLE,
-	ASPECT,
+
+var camera = new THREE.OrthographicCamera ( 
+	WIDTH/ -2, 
+	WIDTH/2, 
+	HEIGHT /2, 
+	HEIGHT / -2,
 	NEAR,
 	FAR);
 
-var radius = 50, segments = 16, rings = 16;
-var sphereGeometry = new THREE.SphereGeometry(radius, segments, rings);
-var sphereMaterial = new THREE.MeshLambertMaterial(
-	{
-		color: 0x00CCDD
-	});
-
-var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-var pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 130;
-
 var scene = new THREE.Scene();
+
+/* ------------Set up geometry*-----------------------*/
+var color = 0x0000C0;
+var platform = new baseChunk(80, color, "img/pop.jpg", "Hip Hop");
+scene.add(platform);
+
+/*-----------------Lighting--------------------*/
+var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.6);
+directionalLight.position.set(0, 1, 0);
+
+hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
+hemiLight.color.setHSL( 0.6, 0.75, 1 );
+hemiLight.groundColor.setHSL( 0.095, 0.5, 1 );
+
+
 scene.add(camera);
-scene.add(sphere);
-scene.add(pointLight);
+scene.add(hemiLight);
+scene.add(new THREE.AmbientLight( 0x00010 ) );
+scene.add(directionalLight);
 camera.position.z = 300;
 
 renderer.setSize(WIDTH, HEIGHT);
 $container.append(renderer.domElement);
 
-
 //---------------Render Stuff-----------------
+
 //Get the browser specific animation frame var.
 var requestAnimationFrame = window.requestAnimationFrame ||  window.webkitRequestAnimationFrame ||
-	window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+ window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
 
 //set it to a variable with same name across browsers
 window.requestAnimationFrame = requestAnimationFrame;
 
 //This function calls itself when browser is ready to animate
 function renderScene(){
+
+	platform.rotation.y = mouseX * 0.005;
+    platform.rotation.x = mouseY * 0.005;
+
 	renderer.render(scene, camera);
 	requestAnimationFrame(renderScene);
 	//console.log("hi");
@@ -66,3 +77,32 @@ function renderScene(){
 
 //Start the render loop.
 requestAnimationFrame(renderScene);
+
+/*Handle various keyboard clicks*/
+
+document.addEventListener('keydown', onKeyDown, false);
+function onKeyDown ( event ) {
+
+	switch ( event.keyCode ) {
+		case 76: /*l*/
+		directionalLight.visible = !directionalLight.visible;
+		break;
+
+	}
+}
+
+
+//For debugging
+document.addEventListener('mousemove', function(event){
+	//update mousex and y
+	mouseX = ( event.clientX - WIDTH/2 );
+    mouseY = ( event.clientY - HEIGHT/2 );
+}, false);
+
+
+
+
+/*
+//http://konrad.strack.pl/blog/image-concatenation-with-imagemagick#.UaMB80BwqrQ
+http://www.elated.com/articles/rotatable-3d-product-boxshot-threejs/
+*/
